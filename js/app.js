12dd7096eb1adf2f23dc2c621220b5b4005e1b6e
Aubrianna Zhu <aubrianna.zhu@gmail.com>
2015-07-03 21:54:46 -0700

    Successfully add filter functionality. Change wikipedia links to show after click, rather than after submit.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,141 @@
+// this is the data model, each data point has a name and latlong coordinates
+var Place = function(data) {
+	var self = this;
+	self.name = data.name;
+	self.latLang = data.latLang;
+};
+
+// this is the ViewModel
+function PlacePageModel(data) {
+	var self = this;
+	this.placeList = ko.observableArray([]);
+
+	places.forEach(function(aplace) {
+		self.placeList.push(new Place(aplace));
+	});
+	this.currentPlace = ko.observable('');
+	
+	this.setPlace = function(chosenPlace) {
+		self.currentPlace(chosenPlace);
+	};
+	
+	// load wikipedia articles when user searches for a place
+	this.loadData = function(place) {
+		var $wikiElem = $('#wikipedia-links');
+		var $greeting = $('#greeting');
+
+		// clear out old data before new request
+		$wikiElem.text("");
+
+		var poiStr=String(place.name);
+
+		var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + poiStr + '&format=json&callback=wikiCallback';
+
+		var wikiRequestTimeout = setTimeout(function() {
+		$wikiElem.text("failed to get wikipedia resources");
+		}, 5000);
+
+		$.ajax({
+			url: wikiUrl,
+			dataType: "jsonp",
+			success: function(response) {
+				var articleList = response[1];
+				for (var i = 0; i < articleList.length; i++) {
+					articleStr = articleList[i];
+					var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+					$wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+				};
+				clearTimeout(wikiRequestTimeout);
+			}
+		});
+		return false;
+	};
+	// create filter for the place list
+	var filters = [{
+			Type: "text",
+			Name: "Name",
+			Value: ko.observable(""),
+			RecordValue: function(record) { return record.name; }
+		}];
+	self.filter = new Filter(filters, this.placeList);
+	self.pager = new PagerModel(self.filter.filteredRecords);
+};
+
+// model that represents the returned records from filtering
+function PagerModel(records) {
+	var self = this;
+	self.records = GetObservableArray(records);
+
+	self.currentPageRecords = ko.computed(function() {
+		return self.records();
+	}).extend({ throttle: 5 });
+};
+
+// 
+function Filter(filters, records) {
+	var self = this;
+	self.records = GetObservableArray(records);
+	self.filters = ko.observableArray(filters);
+	self.activeFilters = ko.computed(function() {
+		var filters = self.filters();
+		var activeFilters = [];
+		for (var index = 0; index < filters.length; index++) {
+			var filter = filters[index];
+			if (filter.Value) {
+				var filterValue = filter.Value();
+				if (filterValue && filterValue != "") {
+					var activeFilter = {
+						Filter: filter,
+						IsFiltered: function(filter, record) {
+							var filterValue = filter.Value();
+							filterValue = filterValue.toUpperCase();
+							
+							var recordValue = filter.RecordValue(record);
+							recordValue = recordValue.toUpperCase();
+							return recordValue.indexOf(filterValue) == -1;
+						};
+					};
+					activeFilters.push(activeFilter);
+				};
+			};
+		};
+		return activeFilters;
+	});
+	self.filteredRecords = ko.computed(function() {
+		var records = self.records();
+		var filters = self.activeFilters();
+		if (filters.length == 0) {
+			return records;
+		};
+		
+		var filteredRecords = [];
+		for (var rIndex = 0; rIndex < records.length; rIndex++) {
+			var isIncluded = true;
+			var record = records[rIndex];
+			for (var fIndex = 0; fIndex < filters.length; fIndex++) {
+				var filter = filters[fIndex];
+				var isFiltered = filter.IsFiltered(filter.Filter, record);
+				if (isFiltered) {
+					isIncluded = false;
+					break;
+				};
+			};
+			if (isIncluded) {
+				filteredRecords.push(record);
+			};
+		};
+		return filteredRecords;
+	}).extend({ throttle: 200 });
+};
+
+
+function GetObservableArray(array) {
+	if (typeof(array) == 'function');{
+		return array;
+	};
+	return ko.observableArray(array);
+};
+
 // save all the points of interest in an array
 var places = [
 {
@@ -21,22 +159,7 @@ var places = [
 	latLang: new google.maps.LatLng(51.5096, -0.1346)
 }];
 
-// this is the model
-var Place = function(data) {
-	this.name = ko.observable(data.name);
-	this.latLang = ko.observable(data.latLang);
-};
-
-var placeFilters = [
-	{
-		Type: "text",
-		Name: "Name",
-		Value: ko.observable(""),
-		//CurrentOption: ko.observable(),
-		RecordValue: function(record) { return record.name; }
-	}
-];
-
+ko.applyBindings(new PlacePageModel(places));
 
 // This initializes the Google Maps section, and puts markers for all the places
 initialize = function() {
@@ -55,6 +178,7 @@ initialize = function() {
 		content: contentString
 	});
 
+	// place a marker on each point of interest
 	var numPlaces = places.length;
 	for (i = 0; i < numPlaces; i ++) {
 		var marker = new google.maps.Marker({
@@ -62,6 +186,7 @@ initialize = function() {
 			map: map,
 			title: places[i].name
 		});
+		// add functionality to distinguish clicked vs unclicked markers
 		google.maps.event.addListener(marker, 'click',(function(markerCopy) {
 			return function() {
 				infowindow.open(map,markerCopy);
@@ -71,134 +196,3 @@ initialize = function() {
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-// load wikipedia articles when user searches for a place
-loadData = function() {
-	var $wikiElem = $('#wikipedia-links');
-	var $greeting = $('#greeting');
-
-	// clear out old data before new request
-	$wikiElem.text("");
-
-	var poiStr = $('#poi').val();
-	$greeting.text('So, you want to explore ' + poiStr + '?');
-
-	var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + poiStr + '&format=json&callback=wikiCallback';
-
-	var wikiRequestTimeout = setTimeout(function() {
-	$wikiElem.text("failed to get wikipedia resources");
-	}, 5000);
-
-	$.ajax({
-		url: wikiUrl,
-		dataType: "jsonp",
-		// jsonp: "callback",
-		success: function(response) {
-			var articleList = response[1];
-
-			for (var i = 0; i < articleList.length; i++) {
-				articleStr = articleList[i];
-				var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-				$wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-			};
-
-			clearTimeout(wikiRequestTimeout);
-		}
-	})
-	return false;
-};
-
-$('#form-container').submit(loadData);
-
-
-var ViewModel = function() {
-	var self = this;
-	this.placeList = ko.observableArray([]);
-
-	places.forEach(function(aplace){
-		self.placeList.push(new Place(aplace));
-	});
-
-	//this.currentPlace = ko.observable(this.placeList()[0]);
-	this.currentPlace = ko.observable('London');
-
-	this.setPlace = function(chosenPlace) {
-		self.currentPlace(chosenPlace);
-	};
-	
-	self.filter = new FilterModel(placeFilters, this.placeList());
-}
-
-var FilterModel = function(filters, records)
-{
-	var self = this;
-	self.records = ko.observableArray(records);
-	self.filters = ko.observableArray(filters);
-	self.activeFilters = ko.computed(function() {
-		var filters = self.filters();
-		var activeFilters = [];
-		for (var index = 0; index < filters.length; index++)
-		{
-			var filter = filters[index];
-			if (filters.Value)
-			{
-				var filterValue = filters.Value();
-				if (filterValue && filterValue != "")
-				{
-					var activeFilter = {
-						Filter: filters,
-						IsFiltered: function(filters, record)
-						{
-							var filterValue = filters.Value();
-							filterValue = filterValue.toUpperCase();
-							
-							var recordValue = filters.RecordValue(record);
-							console.log("logging");
-							console.log(recordValue);
-							recordValue = recordValue.toUpperCase();
-							return recordValue.indexOf(filterValue) == -1;
-						}
-					};
-					activeFilters.push(activeFilter);
-				}
-			}
-		}
-
-		return activeFilters;
-	});
-	self.filteredRecords = ko.computed(function() {
-		var records = self.records();
-		var filters = self.activeFilters();
-		if (filters.length == 0)
-		{
-			return records;
-		}
-		
-		var filteredRecords = [];
-		for (var rIndex = 0; rIndex < records.length; rIndex++)
-		{
-			var isIncluded = true;
-			var record = records[rIndex];
-			for (var fIndex = 0; fIndex < filters.length; fIndex++)
-			{
-				var filter = filters[fIndex];
-				
-				var isFiltered = filter.IsFiltered(filter.Filter, record);
-				if (isFiltered)
-				{
-					isIncluded = false;
-					break;
-				}
-			}
-			
-			if (isIncluded)
-			{
-				filteredRecords.push(record);
-			}
-	}
-
-		return filteredRecords;
-	}).extend({ throttle: 200 });
-}
-
-ko.applyBindings(new ViewModel());
